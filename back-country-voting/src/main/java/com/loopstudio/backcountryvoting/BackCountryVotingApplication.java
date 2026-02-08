4701ad46a2b511cd02da91d3af25eeb352a7d4c6
KrEdRivera <kristhian.eduardo.rivera@gmail.com>
2026-02-08 14:09:08 -0500

    Commit inicial del back

---
+++ back-country-voting/src/main/java/com/loopstudio/backcountryvoting/BackCountryVotingApplication.java
@@ -0,0 +1,13 @@
+package com.loopstudio.backcountryvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCountryVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCountryVotingApplication.class, args);
+	}
+
+}
---
+++ back-country-voting/src/test/java/com/loopstudio/backcountryvoting/BackCountryVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopstudio.backcountryvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCountryVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
